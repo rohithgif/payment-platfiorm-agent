958969d6c50b9ef46a1acadee4d0dfda69b4286a
rohithgif <rohithkarthikeya48851@gmail.com>
2025-06-28 16:48:18 +0530

    initial commit

---
+++ src/main/java/com/hackathon/payment_platform_agent/PaymentPlatformAgentApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.payment_platform_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPlatformAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentPlatformAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/payment_platform_agent/PaymentPlatformAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.payment_platform_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentPlatformAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
